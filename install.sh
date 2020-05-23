0b4c859ab236dbcc4295e63af4854b999751f09f
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2020-05-23 22:08:19 +0100

    Installation Script

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ sudo pacman -S zsh
 echo "zsh version:"
 zsh --version
 chsh -s $(which zsh)
+
+#Installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
